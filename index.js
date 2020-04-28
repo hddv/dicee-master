b74d7e39656e5750279cdcbd98859ffc21720a97
DINH VU Hien <dinhvuhien@MacBook-Pro-de-DINH.local>
2020-04-28 21:38:07 +0200

    set function whoWin() js

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ function randomNumber(){
   return Math.floor(Math.random()*6)+1;
 }
 
+function whoWin(number1, number2){
+  if(number1 > number2){
+    document.querySelector("h1").innerText = "🚩 Player 1 wins!";
+  }
+  else if(number1 < number2){
+    document.querySelector("h1").innerText = "Player 2 wins! 🚩";
+  }
+  else{
+    document.querySelector("h1").innerText = "🚩 Draw 🚩";
+  }
+}
 
+//Set random value to img1
 var randomNumber1 = randomNumber();
 
 document.querySelector("img.img1").setAttribute("src", "images/dice"+randomNumber1+".png");
 
+//Set random value to img2
 var randomNumber2 = randomNumber();
 
 document.querySelector("img.img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+whoWin(randomNumber1, randomNumber2);
