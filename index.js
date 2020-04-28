e8d569b391f72ca1238fbf312b6a9b0ce7e2d8d1
DINH VU Hien <dinhvuhien@MacBook-Pro-de-DINH.local>
2020-04-28 21:27:18 +0200

    change the im2 with js

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function randomNumber(){
 var randomNumber1 = randomNumber();
 
 document.querySelector("img.img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+
+var randomNumber2 = randomNumber();
+
+document.querySelector("img.img2").setAttribute("src", "images/dice"+randomNumber2+".png");
